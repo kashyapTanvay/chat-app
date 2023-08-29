98348f887f2101a1661d4f1947497de30956d140
tanvay <kashyaptanvay@gmail.com>
2023-08-29 11:30:29 -0700

    initianized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDBKQm_CLTnzQrJFaZDvw9jJou58oda91k',
+  authDomain: 'chat-web-app-1dad2.firebaseapp.com',
+  projectId: 'chat-web-app-1dad2',
+  storageBucket: 'chat-web-app-1dad2.appspot.com',
+  messagingSenderId: '690633702054',
+  appId: '1:690633702054:web:5057eea2b2473f17ab1267',
+};
+
+const app = firebase.initializeApp(config);
